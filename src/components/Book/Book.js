b5e66ab0935396bfcc37fe51977e6cdcb5cbc619
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-16 16:57:27 +0600

    private-route-login commit

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -5,10 +5,12 @@ import { UserContext } from '../../App';
 const Book = () => {
     const {bedType} = useParams();
     const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+    
     return (
         <div style={{textAlign: 'center'}}>
             <h1>Hello,{loggedInUser.email}! Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+
         </div>
     );
 };
